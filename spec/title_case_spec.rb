2e95a0cb20896a8026c0c6cd2f26f353b95005c4
Jason Ainsworth and Trevor Fast <trevor@>
2017-04-18 09:57:42 -0700

    add basic files and folder for all your ruby needs

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -3,10 +3,10 @@ require('title_case')
 
 describe('title_case') do
   it('each word of string is indexed into an array') do
-    expect(("nick is hungry").title_case).to eq(["nick", "is", "hungry"])
+    expect(("nick is hungry").title_case).to(eq("Nick Is Hungry"))
   end
 
   it('each indexed word in the array first letter will be Capitalized') do
-    expect(("nick is hungry").title_case).to eq("Nick Is Hungry")
+    expect(("nick is hungry").title_case).to(eq("Nick Is Hungry"))
   end
 end
